66480ad316ba093380ce9172be4897c62f852ac0
孙许 <2572012446@qq.com>
2021-10-20 23:00:09 +0800

    Initial commit

---
+++ src/main/java/com/sunxu/ZuulGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.sunxu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author 孙许
+ * @version 1.0
+ * @date 2021/10/20 22:49
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+}
